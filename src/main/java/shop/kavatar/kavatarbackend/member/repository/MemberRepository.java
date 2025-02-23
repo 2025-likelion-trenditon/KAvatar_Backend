3d3ae1ee4d8f14da9752450de21cd7942a24daf9
dongkyunKim <kyun010713@gmail.com>
2025-02-23 23:14:57 +0900

    feature(#26): 로그인 기능 구현

--- src/main/java/shop/kavatar/kavatarbackend/member/repository/MemberRepository.java
+++ src/main/java/shop/kavatar/kavatarbackend/member/repository/MemberRepository.java
@@ -1,10 +1,12 @@
 package shop.kavatar.kavatarbackend.member.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.kavatar.kavatarbackend.member.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
     List<Member> findTop10ByOrderByPointDesc();
+    Optional<Member> findByEmail(String email);
 }
